da5fd7c738800946b1099b4e387956b2d83caa96
Gerardp <agerardo1397@gmail.com>
2020-08-24 12:17:22 -0500

    slider transition done, right menu done,

--- app.js
+++ app.js
@@ -1,31 +1,93 @@
-//  menu-slider
-//  landing-slider
-const menu_slider = document.getElementById('menu-slider');
-const landing_slider = document.getElementById('landing-slider');
+const autumnB = document.getElementById("autumn")
+const winterB = document.getElementById("winter")
 
-let counter = 0;
-const menu_slider1 = document.querySelector(".imgs-information-imgs").children
-const menu_slider1 = document.querySelector(".imgs-information-imgs").children
+const arrowL = document.querySelector(".fa-arrow-left")
+const arrowR = document.querySelector(".fa-arrow-right")
+// 0 autumn shown 1 winter shown
+let shownM = 0;
+let shownL = 0;
+
+arrowL.addEventListener("click", function () {
+    menu_slider1();
+    landing_slider1();
+});
+
+arrowR.addEventListener("click", function () {
+    menu_slider2();
+    landing_slider2();
+})
+
+autumnB.addEventListener("click", function () {
+    menu_slider1();
+    landing_slider1();
+});
+
+winterB.addEventListener("click", function () {
+    menu_slider2();
+    landing_slider2();
+})
+
+function menu_slider1() {
+
+    if (shownM === 0) return;
+    gsap.to(".menu-slider1", {
+        x: "0%",
+        duration: 1
+    })
+
+    gsap.to(".menu-slider2", {
+        x: "-100%",
+        duration: 1
+    })
+    shownM = 0;
+}
 
 
+function menu_slider2() {
 
-function change(season) {
-    console.log(menu_slider1)
-    if (season === 'a') {
-        counter++;
-        menu_slider1[0].classList.add("img-shown");
-        menu_slider1[0].classList.remove("img-hidden");
+    if (shownM === 1) return;
+    gsap.to(".menu-slider2", {
+        x: "0",
+        duration: 1
+    })
+    gsap.to(".menu-slider1", {
+        x: "100%",
+        duration: 1
+    })
 
-        menu_slider1[1].classList.add("img-hidden");
-        menu_slider1[1].classList.remove("img-shown");
+    shownM = 1;
+}
 
-    } else {
-        counter--;
-        menu_slider1[0].classList.remove("img-shown");
-        menu_slider1[0].classList.add("img-hidden");
+function landing_slider1() {
+    if (shownL === 0) return;
+    gsap.to(".landing-slider1", {
+        x: "0%",
+        duration: 1
+    })
+    gsap.from(".landing-slider1", {
+        scale: 1.5,
+        delay: 0.2,
+        duration: 0.9
+    })
+    gsap.to(".landing-slider2", {
+        x: "-100%",
+        duration: 1
+    })
+    shownL = 0;
+}
 
-        menu_slider1[1].classList.add("img-shown");
-        menu_slider1[1].classList.add("img-hidden");
+function landing_slider2() {
+    if (shownL === 1) return;
 
-    }
+    gsap.to(".landing-slider1", {
+        x: "100%",
+        duration: 1
+    })
+
+    gsap.to(".landing-slider2", {
+        x: "0",
+        duration: 1
+    })
+    shownL = 1;
+
 }
